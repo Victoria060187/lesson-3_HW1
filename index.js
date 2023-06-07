1e7c6751d965d63d61866e95a40c9e86f4e8fdf6
Victoria Laputko <vikalaputko87@gmail.com>
2023-06-07 18:19:26 +0300

    change let to const

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const hours = prompt('How many hours do you want to convert to seconds?')?.trim();
 const seconds = 3600;
-let result;
+
 if(!hours || isNaN(hours)){
     alert ('Error! Incorrect input value!');
 }
@@ -8,6 +8,6 @@ else if (hours <=0){
     alert ('Error! Input value is negative or 0!');
 } 
 else {
-    result = hours*seconds;
+    const result = hours*seconds;
     alert ('Total: ' + result);
 }
